fa7b22b4145bbe875a3e62d355e10832a95cbc4d
Megan McGee <meganpmcgee@gmail.com>
2016-01-20 15:09:20 -0800

    Added sections of MainLayout.jsx usings props

--- client/components/layout/MainLayout.jsx
+++ client/components/layout/MainLayout.jsx
@@ -2,6 +2,11 @@ MainLayout = React.createClass({
   render() {
     return (
       <div>
+        {this.props.header}
+
+        {this.props.content}
+
+        {this.props.footer}
 
       </div>
     );
